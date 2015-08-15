a114c7ae0eab17fd4ba30ffd479fd37b23b4efcc
NickHub4017 <nrv.rox.u@gmail.com>
2015-08-15 10:22:23 +0530

    modify for test new method

--- Testing_script/Testing_script.ino
+++ Testing_script/Testing_script.ino
@@ -10,15 +10,21 @@ Serial.begin(9600);
 void loop() {
 
       digitalWrite(13,LOW);
-      unsigned long t=millis();
-  int x=analogRead(A0);
-  Serial.println(x);
-  /*if(x>300){
-    while(x>300){
-      x=analogRead(A0);
-    }
+      //unsigned long t=millis();
+  int x=getIntervalofOnes();
+ 
+  if(x>300){
+    
 
-
+ //Serial.println(x);
+ 
   }
-*/
+
+}
+
+unsigned long getIntervalofOnes(){
+  unsigned long time=millis();
+  while(analogRead(A0)<=250){
+  }
+  return (millis()-time);
 }
